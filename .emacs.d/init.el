aa29b89eb81a4cc4009439e9e91e2aca7cf7363b
ataka <masayuki.ataka@gmail.com>
2015-05-07 08:48:00 +0900

    Basical Edit Options for Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,3 +18,23 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
+
+;;
+;; Edit Options
+;;
+(setq load-path (cons "~/.emacs.d/site-lisp/" load-path))
+(setq load-path (cons "/usr/local/share/emacs/site-lisp" load-path))
+(setq load-path (cons "/usr/local/share/emacs/site-lisp/tc" load-path))
+
+(setq eval-expression-debug-on-error nil)
+(setq visible-bell t)
+
+(tool-bar-mode -1)
+(column-number-mode t)
+
+(setq line-move-visual nil)
+
+; set fill-column. (default value is 70)
+(setq default-fill-column 80)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
