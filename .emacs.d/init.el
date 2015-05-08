57ed07ac7ec281601a321e87e1193e5fb34db337
ataka <masayuki.ataka@gmail.com>
2015-05-08 23:51:51 +0900

    Add keyboard setting for Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -38,3 +38,25 @@
 (setq default-fill-column 80)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
+
+;;
+;; Key binding
+;;
+(global-set-key [home] 'beginning-of-buffer)
+(global-set-key [end]  'end-of-buffer)
+(global-unset-key "\C-z")
+(global-unset-key "\C-x\C-z")
+
+; C-h prefix
+(define-key help-map "/" 'ispell-complete-word)
+
+; C-c prefix
+(define-key mode-specific-map "c" 'compile)
+(define-key mode-specific-map "g" 'grep)
+(define-key mode-specific-map "o" 'occur)
+(define-key mode-specific-map "u" 'browse-url-at-point)
+(define-key mode-specific-map "U" 'quickurl-browse-url-ask)
+(define-key mode-specific-map "y" 'clipboard-yank)
+
+; C-x prefix
+(define-key ctl-x-map "f" 'font-lock-mode)
