8624d00b1356ab73f6761bb493e2364fce3fb74a
ataka <masayuki.ataka@gmail.com>
2015-05-29 23:51:08 +0900

    Add custom package setting for web-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -174,3 +174,12 @@
 ;; aspell / ispell
 ;;
 (setq ispell-program-name "/usr/local/bin/aspell")
+
+;;
+;; web-mode
+;;
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (setq web-mode-markup-indent-offset 2)))
