c836ece73e5d47eebee34bdce788648f3d4c9206
Masayuki Ataka <masayuki.ataka@gmail.com>
2025-01-05 11:38:34 +0900

    AereSpace: Add Document and default settings

--- link.sh
+++ link.sh
@@ -4,7 +4,7 @@
 stow --target ~ screen tmux zsh
 
 # ~/.config に置く設定ファイル
-stow --dotfiles --target ~ bat git karabiner nushell starship wezterm
+stow --dotfiles --target ~ aerospace bat git karabiner nushell starship wezterm
 
 # ~/Library/Developer/Xcode に置く設定ファイル
 stow --target ~/Library/Developer/Xcode Xcode
