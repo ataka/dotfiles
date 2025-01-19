5a7fcb249f6b002b1afa2757b1b3e6bca76d8166
Masayuki Ataka <masayuki.ataka@gmail.com>
2025-01-20 08:08:44 +0900

    sketchybarrc: Fix spaces.sh to work with AeroSpace

--- sketchybar/dot-config/sketchybar/items/spaces.sh
+++ sketchybar/dot-config/sketchybar/items/spaces.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-SPACE_SIDS=(1 2 3 4 5 6 7 8 9)
+sketchybar --add event aerospace_workspace_change
 
-for sid in "${SPACE_SIDS[@]}"
+for sid in $(aerospace list-workspaces --all)
 do
-  sketchybar --add space space.$sid left                                 \
-             --set space.$sid space=$sid                                 \
-                              icon=$sid                                  \
-                              label.font="sketchybar-app-font:Regular:16.0" \
-                              label.padding_right=20                     \
-                              label.y_offset=-1                          \
-                              script="$PLUGIN_DIR/space.sh"
+    sketchybar --add item space.$sid left \
+        --subscribe space.$sid aerospace_workspace_change \
+        --set space.$sid \
+              icon.padding_right=0 \
+              label.padding_left=0 \
+              background.color=$BORDER_COLOR \
+              background.corner_radius=5 \
+              background.height=20 \
+              background.drawing=off \
+              label="$sid" \
+              click_script="aerospace workspace $sid" \
+              script="$CONFIG_DIR/plugins/space.sh $sid"
 done
-
-sketchybar --add item space_separator left                             \
-           --set space_separator icon="􀆊"                                \
-                                 icon.color=$ACCENT_COLOR \
-                                 icon.padding_left=4                   \
-                                 label.drawing=off                     \
-                                 background.drawing=off                \
-                                 script="$PLUGIN_DIR/space_windows.sh" \
-           --subscribe space_separator space_windows_change
+#   sketchybar --add item space.$sid left \
+#              --subscribe space.$sid aerospace_workspace_change \
+#              --set space.$sid label=$sid \
+#                               background.color=$ACCENT_COLOR \
+#                               background.corner_radius=5 \
+#                               background.height=20 \
+#                               background.drawing=off \
+#                               label.font="sketchybar-app-font:Regular:16.0" \
+#                               label.y_offset=-1 \
+#                               click_script="aerospace workspace $sid" \
+#                               script="$PLUGIN_DIR/space.sh $sid" \
+# done
--- sketchybar/dot-config/sketchybar/plugins/space.sh
+++ sketchybar/dot-config/sketchybar/plugins/space.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # The $SELECTED variable is available for space components and indicates if
 # the space invoking this script (with name: $NAME) is currently selected:
@@ -6,13 +6,13 @@
 
 source "$CONFIG_DIR/colors.sh"
 
-if [ $SELECTED = true ]; then
+if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
   sketchybar --set $NAME background.drawing=on \
                          background.color=$ACCENT_COLOR \
                          label.color=$BAR_COLOR \
                          icon.color=$BAR_COLOR
 else
   sketchybar --set $NAME background.drawing=off \
-                         label.color=$ACCENT_COLOR \
-                         icon.color=$ACCENT_COLOR
+                         label.color=$WHITE \
+                         icon.color=$WHITE
 fi
