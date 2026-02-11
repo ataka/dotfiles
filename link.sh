a774045359f747e590aa1d355bbe7e785b28c544
Masayuki Ataka <masayuki.ataka@gmail.com>
2026-02-11 21:10:31 +0900

    tcode/config.json: Add settings for MacTcode

--- link.sh
+++ link.sh
@@ -9,5 +9,8 @@ stow $argv --dotfiles --target ~ aerospace atuin bat git jankyborders karabiner 
 # ~/Library/Developer/Xcode に置く設定ファイル
 stow $argv --target ~/Library/Developer/Xcode Xcode
 
+# MacTcode
+stow $argv --target ~/Library/Containers/jp.mad-p.inputmethod.MacTcode/Data/Library/Application\ Support/MacTcode tcode
+
 # Mouseless
 stow $argv --target ~/Library/Containers/net.sonuscape.mouseless/Data/.mouseless/configs mouseless
